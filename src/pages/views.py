34e229edeb2694b31f2c563fb01dd25a7dd96c9d
mboy1011 <lyjieme@gmail.com>
2019-04-24 21:57:14 +0800

    Added New Pages for the Project.

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request,*args, **kwargs): #*args, **kwargs
-    # return HttpResponse("<h1>Home Page</h1>")
     return render(request,"home.html",{})
 def about_view(request,*args, **kwargs): #*args, **kwargs
-    return render(request,"about.html",{})
+    my_context = {
+        "my_text" :  "This is about us.",
+        "this_is_true":True,
+        "my_number" : 123,
+        "my_list" : [1313, 4231, 312, "Abc"]
+    }
+    return render(request,"about.html",my_context)
 
 def contact_view(request,*args, **kwargs): #*args, **kwargs
     return render(request,"contact.html",{})
